2cab6ccdfd7cef0be794e4fd119c6a6e3218cf38
Joshua Kunst <jbkunst@gmail.com>
2022-05-20 12:25:02 -0400

    conf pkgdown

--- dev/usethis.R
+++ dev/usethis.R
@@ -1,3 +1,4 @@
+# skeleton and some musts -------------------------------------------------
 usethis::create_package("../klasset/")
 
 usethis::use_readme_md()
@@ -6,9 +7,16 @@ usethis::use_mit_license()
 
 usethis::use_github_action_check_standard()
 
+
+# pkgdown -----------------------------------------------------------------
 usethis::use_github_action("pkgdown")
 
+usethis::edit_r_buildignore() # add docs folder to Rbuilignore to test
 
+pkgdown::build_site_github_pages()
+
+
+# developing package ------------------------------------------------------
 usethis::use_package("tibble")
 usethis::use_package("ggplot2")
 usethis::use_package("stringr")
